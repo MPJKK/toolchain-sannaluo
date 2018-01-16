8cff45881e1a85d7530afb617db36f5e133763cd
Sanna Luostarinen <sannaluo@ety-a2123b-18.edu.wks.metropolia.fi>
2018-01-16 11:41:44 +0200

    valmis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MainComponent } from './main/main.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    MainComponent,
+    MainContentComponent,
+    AsideComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
